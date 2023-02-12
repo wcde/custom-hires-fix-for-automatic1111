779f18a3eee34a51577347ed04642a64e9a340f3
wcde <65939220+wcde@users.noreply.github.com>
2023-02-12 16:19:51 +0300

    rewriting script, seed fix

--- custom_processing.py
+++ custom_processing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import types
 from PIL import Image
 import modules.images as images
 
@@ -12,29 +13,30 @@ opt_f = 8
 
 
 class SDProcessing(processing.StableDiffusionProcessingTxt2Img):
-    def __init__(self, p: processing.StableDiffusionProcessingTxt2Img, hr_passes, hr_custom_upscaler, cfg_per_pass):
+    def __init__(self, p: processing.StableDiffusionProcessingTxt2Img, first_upscaler, second_upscaler):
         super().__init__(sd_model=shared.sd_model, outpath_samples=opts.outdir_samples or opts.outdir_txt2img_samples,
                          outpath_grids=opts.outdir_grids or opts.outdir_txt2img_grids, prompt=p.prompt,
                          negative_prompt=p.negative_prompt, seed=p.seed, subseed=p.subseed,
                          subseed_strength=p.subseed_strength,
                          seed_resize_from_h=p.seed_resize_from_h, seed_resize_from_w=p.seed_resize_from_w,
-                         sampler_name=p.sampler,
+                         sampler_name=p.sampler_name,
                          batch_size=p.batch_size, n_iter=p.n_iter, steps=p.steps, cfg_scale=p.cfg_scale,
                          width=p.width, height=p.height, restore_faces=p.restore_faces, tiling=p.tiling,
                          enable_hr=p.enable_hr, hr_upscaler=p.hr_upscaler, hr_second_pass_steps=p.hr_second_pass_steps,
                          denoising_strength=p.denoising_strength, hr_scale=p.hr_scale)
         self.hr_resize_x = p.hr_resize_x
         self.hr_resize_y = p.hr_resize_y
-        self.hr_passes = hr_passes
-        self.hr_custom_upscaler = hr_custom_upscaler
-        self.cfg_per_pass = cfg_per_pass
         self.hr_steps = p.hr_second_pass_steps
+        self.pass_num = 2 if first_upscaler != 'None' or second_upscaler != 'None' else 1
+        self.first_upscaler = first_upscaler
+        self.second_upscaler = second_upscaler
+        self.cfg_per_pass = 0
+        self.init(None, None, None)
 
     def sample(self, conditioning, unconditional_conditioning, seeds, subseeds, subseed_strength, prompts):
         self.sampler = sd_samplers.create_sampler(self.sampler_name, self.sd_model)
         latent_scale_mode = shared.latent_upscale_modes.get(self.hr_upscaler, None) \
             if self.hr_upscaler is not None else shared.latent_upscale_modes.get(shared.latent_upscale_default_mode, "nearest")
-
 
         x = processing.create_random_tensors([opt_C, self.height // opt_f, self.width // opt_f], seeds=seeds,
                                              subseeds=subseeds,
@@ -46,63 +48,74 @@ class SDProcessing(processing.StableDiffusionProcessingTxt2Img):
 
         if not self.enable_hr:
             return samples
+        add_target_width = (self.hr_upscale_to_x - self.width) / self.pass_num
+        add_target_height = (self.hr_upscale_to_y - self.height) / self.pass_num
 
-        add_target_width = (self.hr_upscale_to_x - self.width) / self.hr_passes
-        add_target_height = (self.hr_upscale_to_y - self.height) / self.hr_passes
+        for stage in range(1, self.pass_num + 1):
+            self.hr_second_pass_steps = self.hr_steps + max(self.steps - self.hr_steps, 0) // 2 if stage == 1 else self.hr_steps
+            self.cfg_scale = self.cfg_scale + self.cfg_per_pass
 
-        for current_pass in range(1, self.hr_passes + 1):
-            self.hr_second_pass_steps = int(self.hr_steps + self.hr_steps / current_pass)
-            self.cfg_scale = self.cfg_scale + (2 * self.cfg_per_pass)
-
-            if current_pass == self.hr_passes:
+            if stage == self.pass_num:
                 target_width = self.hr_upscale_to_x
                 target_height = self.hr_upscale_to_y
             else:
-                target_width = int(self.width + add_target_width * current_pass)
-                target_height = int(self.height + add_target_height * current_pass)
+                target_width = int(self.width + add_target_width * stage)
+                target_height = int(self.height + add_target_height * stage)
 
             scale = target_width / max(self.seed_resize_from_w, self.width)
 
-            if latent_scale_mode is None:
-                decoded_samples = processing.decode_first_stage(self.sd_model, samples)
-                lowres_samples = torch.clamp((decoded_samples + 1.0) / 2.0, min=0.0, max=1.0)
+            upscaler = self.first_upscaler if stage == 1 and self.first_upscaler != 'None' else self.second_upscaler
+            if upscaler == 'From webui':
+                if latent_scale_mode is not None:
+                    samples = torch.nn.functional.interpolate(samples,
+                                                              size=(target_height // opt_f, target_width // opt_f),
+                                                              mode=latent_scale_mode["mode"],
+                                                              antialias=latent_scale_mode["antialias"])
+                    if getattr(self, "inpainting_mask_weight", shared.opts.inpainting_mask_weight) < 1.0:
+                        image_conditioning = self.img2img_image_conditioning(processing.decode_first_stage(self.sd_model, samples),
+                                                                             samples)
+                    else:
+                        image_conditioning = self.txt2img_image_conditioning(samples)
+                else:
+                    decoded_samples = processing.decode_first_stage(self.sd_model, samples)
+                    lowres_samples = torch.clamp((decoded_samples + 1.0) / 2.0, min=0.0, max=1.0)
 
-                batch_images = []
-                for i, x_sample in enumerate(lowres_samples):
-                    x_sample = 255. * np.moveaxis(x_sample.cpu().numpy(), 0, 2)
-                    x_sample = x_sample.astype(np.uint8)
-                    image = Image.fromarray(x_sample)
+                    batch_images = []
+                    for i, x_sample in enumerate(lowres_samples):
+                        x_sample = 255. * np.moveaxis(x_sample.cpu().numpy(), 0, 2)
+                        x_sample = x_sample.astype(np.uint8)
+                        image = Image.fromarray(x_sample)
 
-                    image = images.resize_image(0, image, target_width, target_height, upscaler_name=self.hr_upscaler)
-                    image = np.array(image).astype(np.float32) / 255.0
-                    image = np.moveaxis(image, 2, 0)
-                    batch_images.append(image)
+                        image = images.resize_image(0, image, target_width, target_height,
+                                                    upscaler_name=self.hr_upscaler)
+                        image = np.array(image).astype(np.float32) / 255.0
+                        image = np.moveaxis(image, 2, 0)
+                        batch_images.append(image)
 
-                decoded_samples = torch.from_numpy(np.array(batch_images))
-                decoded_samples = decoded_samples.to(shared.device)
-                decoded_samples = 2. * decoded_samples - 1.
+                    decoded_samples = torch.from_numpy(np.array(batch_images))
+                    decoded_samples = decoded_samples.to(shared.device)
+                    decoded_samples = 2. * decoded_samples - 1.
 
-                samples = self.sd_model.get_first_stage_encoding(self.sd_model.encode_first_stage(decoded_samples))
+                    samples = self.sd_model.get_first_stage_encoding(self.sd_model.encode_first_stage(decoded_samples))
 
-                image_conditioning = self.img2img_image_conditioning(decoded_samples, samples)
+                    image_conditioning = self.img2img_image_conditioning(decoded_samples, samples)
+
             else:
-                if self.hr_custom_upscaler == "nearest-exact":
+                if upscaler == "Latent(nearest-exact)":
                     samples = torch.nn.functional.interpolate(samples, size=(target_height // opt_f, target_width // opt_f),
                                                               mode='nearest-exact')
                 else:
                     samples = resize(samples, scale_factors=(scale, scale),
-                                     interp_method=getattr(interp_methods, self.hr_custom_upscaler))
+                                     interp_method=getattr(interp_methods, 'lanczos2'))
 
                 if getattr(self, "inpainting_mask_weight", shared.opts.inpainting_mask_weight) < 1.0:
                     image_conditioning = self.img2img_image_conditioning(processing.decode_first_stage(self.sd_model,
-                                                                                                       samples), samples)
+                                                                         samples), samples)
                 else:
                     image_conditioning = self.txt2img_image_conditioning(samples)
 
             shared.state.nextjob()
-            if current_pass == 1:
-                self.sampler = sd_samplers.create_sampler(self.sampler_name, self.sd_model)
-
+            self.sampler = sd_samplers.create_sampler(self.sampler_name, self.sd_model)
             self.noise = processing.create_random_tensors(samples.shape[1:], seeds=seeds, subseeds=subseeds,
                                                           subseed_strength=subseed_strength,
                                                           p=self, seed_resize_from_w=self.seed_resize_from_w,
@@ -113,6 +126,7 @@ class SDProcessing(processing.StableDiffusionProcessingTxt2Img):
                                                   steps=self.hr_second_pass_steps or self.steps,
                                                   image_conditioning=image_conditioning)
 
+            devices.torch_gc()
             self.seed_resize_from_w = target_width
             self.seed_resize_from_h = target_height
 
--- scripts/custom_hires_fix.py
+++ scripts/custom_hires_fix.py
@@ -1,119 +1,121 @@
 import torch
-from modules import scripts, script_callbacks, processing
+from modules import scripts, script_callbacks, processing, shared
 import gradio as gr
 from k_diffusion import sampling
 import custom_processing
 
 try:
-   import resize_right
+    import resize_right
 except Exception:
     import pip
+
     if hasattr(pip, 'main'):
         pip.main(['install', 'resize-right'])
     else:
         pip._internal.main(['install', 'resize-right'])
 
+
 class CustomHiresFix(scripts.Script):
     def __init__(self):
         super().__init__()
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.hr_prog = None
-        self.force_details = None
-        self.cfg_per_step = None
-        self.steps = None
-        self.pr = None
-        self.hr_sampler = None
-        self.first_pass_started = True
-        self.first_pass_processing = False
-        self.second_pass_processing = False
-        self.cfg = None
+        self.proc = None
         self.callback_set = False
-    
+        self.stage = 'Gen'
+        self.original_denoise = 0.0
+        self.original_cfg = 0
+        self.first_cfg = 0
+        self.second_cfg = 0
+        self.first_sampler = ''
+        self.second_sampler = ''
+        self.first_noise_scheduler = ''
+        self.second_noise_scheduler = ''
+        self.first_denoise = 0.0
+        self.second_denoise = 0.0
+
     def title(self):
         return "Custom hires fix"
 
     def show(self, is_img2img):
-        return not is_img2img
+        if not is_img2img:
+            return scripts.AlwaysVisible
+        else:
+            return False
 
     def ui(self, is_img2img):
-        with gr.Blocks():
+        with gr.Accordion(label='Custom hires fix', open=False):
             with gr.Row():
-                force_details = gr.Slider(minimum=0, maximum=10, step=1, label="Force details", value=5)
-                hr_prog = gr.Checkbox(label="Smoothing", value=False)
-                hr_passes = gr.Slider(minimum=1, maximum=3, step=1, label="Passes", value=2)
-        with gr.Accordion("Extra", open=False):
+                first_upscaler = gr.Dropdown(['From webui', 'Latent(lanczos2)', 'Latent(nearest-exact)', 'None'],
+                                             label='First upscaler', value='From webui')
+                second_upscaler = gr.Dropdown(['From webui', 'Latent(lanczos2)', 'Latent(nearest-exact)', 'None'],
+                                              label='Second upscaler', value='Latent(lanczos2)')
             with gr.Row():
-                noise_sheduler = gr.Dropdown(['Karras', 'VP', 'Exp'],
-                                                label='Noise Scheduler', value='Karras', type='value')
-                hr_custom_upscaler = gr.Dropdown(['lanczos2', 'lanczos3', 'nearest-exact'],
-                                          label='Latent upscaler', value='lanczos2', type='value')
-                beta = gr.Slider(minimum=1, maximum=20, step=1, label="VP Beta", value=10)
+                first_cfg = gr.Slider(minimum=0, maximum=10, step=1, label="CFG scale boost (1)", value=3)
+                second_cfg = gr.Slider(minimum=0, maximum=10, step=1, label="CFG scale boost (2)", value=5)
             with gr.Row():
-                hr_sampler = gr.Dropdown(['DPM++ 2M', 'Euler a', 'Euler', 'DDIM'],
-                                         label='Hires Sampler', value='DPM++ 2M', type='value')
-                hr_noise_sheduler = gr.Dropdown(['Poly Exp', 'VP', 'Karras'],
-                                                label='Hires Noise Scheduler', value='Poly Exp', type='value')
-            with gr.Blocks():
-                with gr.Row():
-                    hr_sigma = gr.Slider(minimum=1, maximum=25, step=1, label="HR Sigma", value=7)
-                    hr_rho = gr.Slider(minimum=0.5, maximum=20, step=0.5, label="HR Rho (Karras and Poly)", value=0.5)
-                with gr.Row():
-                    hr_beta = gr.Slider(minimum=0.5, maximum=20, step=0.5, label="HR Beta max (VP)", value=7)
-                    hr_beta_min = gr.Slider(minimum=0.1, maximum=20, step=0.1, label="HR Beta min (VP)", value=0.1)
-                with gr.Row():
-                    hr_sigma_min = gr.Slider(minimum=0.01, maximum=3, step=0.01, label="HR Sigma min", value=0.01)
-                    hr_eps = gr.Slider(minimum=0.001, maximum=0.5, step=0.001, label="HR Esp (VP)", value=0.001)
+                first_denoise = gr.Slider(minimum=-0.3, maximum=0.3, step=0.01, label="Denoise strength shift (1)", value=-0.05)
+                second_denoise = gr.Slider(minimum=-0.3, maximum=0.3, step=0.01, label="Denoise strength shift (2)", value=0.0)
+            with gr.Row():
+                first_sampler = gr.Dropdown(['DPM++ 2M', 'Euler a'], label='Sampler (1)', value='DPM++ 2M')
+                second_sampler = gr.Dropdown(['DPM++ 2M', 'Euler a'], label='Sampler (2)', value='DPM++ 2M')
+            with gr.Row():
+                first_noise_scheduler = gr.Dropdown(['High denoising', 'Low denoising'], label='Noise scheduler (1)', value='High denoising')
+                second_noise_scheduler = gr.Dropdown(['High denoising', 'Low denoising'], label='Noise scheduler (2)', value='Low denoising')
+            with gr.Row():
+                disable = gr.Checkbox(label='Disable extension', value=False)
 
-        return [hr_noise_sheduler, hr_sigma_min, hr_sigma, hr_rho, hr_beta, hr_beta_min, hr_eps, noise_sheduler, beta,
-                hr_sampler, hr_passes, hr_custom_upscaler, force_details, hr_prog]
-    
+        return [first_upscaler, second_upscaler, first_cfg, second_cfg, first_denoise, second_denoise,
+                first_sampler, second_sampler, first_noise_scheduler, second_noise_scheduler, disable]
+
     def denoise_callback(self, p: script_callbacks.CFGDenoiserParams):
-        if p.sampling_step == (self.steps - 2) and self.first_pass_processing:  # UI bug
-            self.pr.sampler_name = self.hr_sampler
-        if self.second_pass_processing and self.hr_prog:
-            self.pr.cfg_scale = self.pr.cfg_scale - (self.force_details * 0.14)
-        if self.second_pass_processing and p.sampling_step != 0 and not self.hr_prog:
-            self.pr.cfg_scale = self.cfg
-
-
-    def run(self, p: processing.StableDiffusionProcessingTxt2Img,
-                hr_noise_sheduler, hr_sigma_min, hr_sigma, hr_rho, hr_beta, hr_beta_min, hr_eps, noise_sheduler,
-                beta, hr_sampler, hr_passes, hr_custom_upscaler, force_details, hr_prog):
-        p = custom_processing.SDProcessing(p, hr_passes=hr_passes, hr_custom_upscaler=hr_custom_upscaler,
-                                           cfg_per_pass=force_details)
-        self.force_details = force_details
-        self.cfg = p.cfg_scale
-        self.hr_prog = hr_prog
-
         def denoiser_override(n):
-            if self.first_pass_started:
-                self.first_pass_started = False
-                self.first_pass_processing = True
-                if noise_sheduler == 'Exp':
-                    return sampling.get_sigmas_exponential(n, 0.1, 10, self.device)
-                if noise_sheduler == 'Karras':
-                    return sampling.get_sigmas_karras(n, 0.1, 10, 7, self.device)
-                if noise_sheduler == 'VP':
-                    return sampling.get_sigmas_vp(n, beta, 0.1, 0.001).to(self.device)
-            else:
-                self.first_pass_processing = False
-                self.second_pass_processing = True
-                if hr_noise_sheduler == 'Karras':
-                    return sampling.get_sigmas_karras(n, hr_sigma_min, hr_sigma, hr_rho, self.device)
-                if hr_noise_sheduler == 'VP':
-                    return sampling.get_sigmas_vp(n, hr_beta, hr_beta_min, hr_eps).to(self.device)
-                if hr_noise_sheduler == 'Poly Exp':
-                    return sampling.get_sigmas_polyexponential(n, hr_sigma_min, hr_sigma, hr_rho, self.device)
+            scheduler = self.first_noise_scheduler if self.stage == 'Stage 1' else self.second_noise_scheduler
+            return sampling.get_sigmas_polyexponential(n, 0.01, 15 if scheduler == 'High denoising' else 7, 0.5, self.device)
 
-        p.sampler_noise_scheduler_override = denoiser_override
-        self.steps = p.steps
-        self.pr = p
-        self.hr_sampler = hr_sampler
-        self.first_pass_started = True
-        self.first_pass_processing = False
-        self.second_pass_processing = False
+        is_last_step = p.sampling_step == p.total_sampling_steps - 2
+        if self.stage == 'Gen' and is_last_step:
+            self.stage = 'Stage 1'
+
+        if self.stage == 'Stage 1' and is_last_step:
+            self.proc.denoising_strength = self.original_denoise + self.first_denoise
+            self.proc.cfg_per_pass = self.first_cfg
+            self.stage = 'Stage 2'
+        elif self.stage == 'Stage 2' and is_last_step:
+            self.proc.denoising_strength = self.original_denoise + self.second_denoise
+            self.proc.cfg_per_pass = self.second_cfg
+            shared.disable_custom_hires_fix = False   # for xyz plot
+            self.stage = 'Completed'
+
+        if p.sampling_step != 0 and not is_last_step:
+            self.proc.cfg_scale = self.original_cfg - max((self.first_cfg // 3) if self.stage == 'Stage 1' else (self.second_cfg // 3), 3)
+
+        self.proc.sampler_noise_scheduler_override = denoiser_override
+        self.proc.sampler_name = self.first_sampler if self.stage == 'Stage 1' else self.second_sampler
+
+    def process(self, p: processing.StableDiffusionProcessingTxt2Img,
+                first_upscaler, second_upscaler, first_cfg, second_cfg, first_denoise, second_denoise,
+                first_sampler, second_sampler, first_noise_scheduler, second_noise_scheduler, disable):
+        if disable:
+            return
+        if hasattr(shared, 'disable_custom_hires_fix'):   # for xyz plot
+            if shared.disable_custom_hires_fix:
+                return
+        if self.stage == 'Completed':
+            self.stage = 'Gen'
+        self.first_cfg = first_cfg * 2
+        self.second_cfg = second_cfg * 2
+        self.first_sampler = first_sampler
+        self.second_sampler = second_sampler
+        self.first_noise_scheduler = first_noise_scheduler
+        self.second_noise_scheduler = second_noise_scheduler
+        self.first_denoise = first_denoise
+        self.second_denoise = second_denoise
+        custom = custom_processing.SDProcessing(p, first_upscaler, second_upscaler)
+        p.sample = custom.sample
+        self.proc = custom
         if not self.callback_set:
             script_callbacks.on_cfg_denoiser(self.denoise_callback)
+            self.original_cfg = p.cfg_scale
+            self.original_denoise = p.denoising_strength
+            self.stage = 'Gen'
             self.callback_set = True
-
-        return processing.process_images(p)
